07538d5c17a079cf50ec36d3eaa9c6e385ad2147
Plamen Pavlov <you@example.com>
2019-04-08 19:27:54 +0300

    change password doesnt work

---
+++ trysmt/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :password_resets
+  resources :documents
+  root to: 'pages#home'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
